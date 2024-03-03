d93cfc0f29d95bf1116be153c389cb961b2bc042
Juanca <riberojuanca@gmail.com>
2024-03-03 18:08:24 -0300

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,20 @@
+import { Inter, Roboto_Condensed } from "next/font/google";
+import "./globals.css";
+
+const roboto = Roboto_Condensed({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const metadata = {
+  title: "Juanca Ribero",
+  description: "Uruguayan development",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={roboto.className}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import { CiSun } from "react-icons/ci";
+import { CiDark } from "react-icons/ci";
+import { CiMail } from "react-icons/ci";
+
+function page() {
+  return (
+    <main>
+      <header>
+        <h1 className="title">Juanca Ribero</h1>
+        <div>
+          <a href="">
+            <CiSun />
+          </a>
+          <a href="">
+            <CiMail />
+          </a>
+          <a href=""></a>
+        </div>
+      </header>
+      <article>
+        <div className="container">
+          <div className="sub-container">
+            <section className="sub-container-section">
+              <h2 className="title-section">Titulo1</h2>
+              <div className="body-section">
+                <p>Parrafo1</p>
+                <div className="skills">
+                  <div className="skill">
+                    <p>sadasdk</p>
+                    <p>+7</p>
+                    <p>asdadadasd</p>
+                  </div>
+                  <div className="skill">
+                    <p>sadasdk</p>
+                    <p>+7</p>
+                    <p>asdadadasd</p>
+                  </div>
+                  <div className="skill">
+                    <p>sadasdk</p>
+                    <p>+7</p>
+                    <p>asdadadasd</p>
+                  </div>
+                </div>
+                <p>parrafo2</p>
+              </div>
+            </section>
+          </div>
+          <div className="sub-container">
+            <section className="sub-container-section">
+              <h2 className="title-section">Titulo1</h2>
+              <div className="body-section">
+                <p>Parrafo1</p>
+                <div className="skills">
+                  <div className="skill">
+                    <p>sadasdk</p>
+                    <p>+7</p>
+                    <p>asdadadasd</p>
+                  </div>
+                  <div className="skill">
+                    <p>sadasdk</p>
+                    <p>+7</p>
+                    <p>asdadadasd</p>
+                  </div>
+                  <div className="skill">
+                    <p>sadasdk</p>
+                    <p>+7</p>
+                    <p>asdadadasd</p>
+                  </div>
+                </div>
+                <p>parrafo2</p>
+              </div>
+            </section>
+          </div>
+        </div>
+      </article>
+      <footer className="footer">
+        <p>Juanca</p>
+        <p>Contacto</p>
+      </footer>
+    </main>
+  );
+}
+
+export default page;
