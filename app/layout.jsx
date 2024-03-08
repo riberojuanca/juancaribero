66dde04e2201771a532e520a2dba2d41f82dca0b
Juanca <riberojuanca@gmail.com>
2024-03-08 19:16:30 -0300

    Head section added to layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,15 +1,9 @@
 import { Inter } from "next/font/google";
-import { Yantramanav } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-});
-
-const yantramanav = Yantramanav({
-  subsets: ["latin"],
-  weight: ["100", "300", "400", "500", "700", "900"],
 });
 
 export const metadata = {
@@ -20,7 +14,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <link rel="icon" href="/favicon.ico" sizes="any" />
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
